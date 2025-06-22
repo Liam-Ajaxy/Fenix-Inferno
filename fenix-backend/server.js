701b7b0f6830803986bed7e08500fba966404f76
Liam-Ajaxy <Liam-Ajaxy@users.noreply.github.com>
2025-06-22 17:31:44 +0200

    Update cors local and host

--- fenix-backend/server.js
+++ fenix-backend/server.js
@@ -10,7 +10,10 @@ const jwt = require('jsonwebtoken');
 const path = require('path');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5500', 'http://127.0.0.1:5500'],
+  credentials: true
+}));
 app.use(express.json());
 
 // === ENV VARIABLES ===
